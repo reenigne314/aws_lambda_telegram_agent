f9ed82baf03c124db35026c6d526d2657180886a
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-10-14 17:02:35 +0200

    fix: final fix of MongoDB

--- src/telegram_agent_aws/infrastructure/mongodb_utils.py
+++ src/telegram_agent_aws/infrastructure/mongodb_utils.py
@@ -1,4 +1,3 @@
-import certifi
 from functools import lru_cache
 
 from pymongo import MongoClient
@@ -8,6 +7,5 @@ from telegram_agent_aws.config import settings
 @lru_cache(maxsize=1)
 def get_mongodb_client():
     return MongoClient(
-        settings.MONGODB_CONNECTION_STRING,
-        tlsCAFile=certifi.where(),
+        settings.MONGODB_CONNECTION_STRING
     )
