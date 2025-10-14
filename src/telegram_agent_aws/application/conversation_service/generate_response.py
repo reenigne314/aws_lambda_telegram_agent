c2f0411b7e9c37756f3d7bc4de0830efc8cc4a5b
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-10-14 12:17:00 +0200

    feat: add 180s timeout

--- src/telegram_agent_aws/application/conversation_service/generate_response.py
+++ src/telegram_agent_aws/application/conversation_service/generate_response.py
@@ -1,14 +1,8 @@
 from langgraph.checkpoint.memory import MemorySaver
 
 from telegram_agent_aws.application.conversation_service.workflow.graph import create_workflow_graph
-from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
-@retry(
-    stop=stop_after_attempt(3),
-    wait=wait_exponential(multiplier=1, min=2, max=10),
-    retry=retry_if_exception_type(Exception),
-    reraise=True
-)
+
 def get_agent_response(payload: dict, user_id: int):
     config = {
         "configurable": {"thread_id": str(user_id)}
