8a856c5fca8a2271aca1890fa12289695f0be18a
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-10-23 08:07:41 +0200

    fix: Qdrant collection creation

--- src/telegram_agent_aws/application/rag_indexing_service/index_documents.py
+++ src/telegram_agent_aws/application/rag_indexing_service/index_documents.py
@@ -22,12 +22,12 @@ def index_documents():
     all_splits = generate_split_documents()
     embeddings = OpenAIEmbeddings(model=settings.EMBEDDING_MODEL, api_key=settings.OPENAI_API_KEY)
 
-    vector_store = QdrantVectorStore(
-        client=get_qdrant_client(),
-        collection_name="telegram_agent_aws_collection",
+    QdrantVectorStore.from_documents(
+        documents=all_splits,
         embedding=embeddings,
+        url=settings.QDRANT_URL,
+        api_key=settings.QDRANT_API_KEY,
+        collection_name="telegram_agent_aws_collection",
     )
-
-    vector_store.add_documents(all_splits)
 
     logger.info("Documents indexed successfully.")
